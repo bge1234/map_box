934ac5e0814c36a564acdddcbeb97cb8a665367d
Ben Ebel <bge1234@gmail.com>
2016-01-20 15:18:47 -0700

    removed unnecessary key

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ var request = require('request');
 /* GET home page. */
 router.get('/', function(req, res, next) {
 
-  request("https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=AIzaSyA0gcQALZj7iUV0P20a9sHNQvA6amQ01b0", function (error, response, body) {
+  request("https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA", function (error, response, body) {
     if (!error && response.statusCode == 200) {
       var jase = JSON.parse(body);
       var lat = jase["results"][0]["geometry"]["location"]["lat"];
