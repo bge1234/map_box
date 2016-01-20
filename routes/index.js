c931b724f27316f74f3b77c6e7988ff98839e110
Ben Ebel <bge1234@gmail.com>
2016-01-20 15:10:37 -0700

    added geocoding

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
+var request = require('request');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+  request("https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=AIzaSyA0gcQALZj7iUV0P20a9sHNQvA6amQ01b0", function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      var jase = JSON.parse(body);
+      var lat = jase["results"][0]["geometry"]["location"]["lat"];
+      var lng = jase["results"][0]["geometry"]["location"]["lng"];
+      res.render('index', { title: 'Express', lat: lat, lng: lng });
+    }
+  });
 });
 
 module.exports = router;
